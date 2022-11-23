22df1413e1d1a04355c8517ad1c290d190395165
michalubczynski <michlub322@student.polsl.pl>
2022-11-23 11:29:30 +0100

    Ok

---
+++ src/MetodaSzablonowa/Kuchnia.java
@@ -0,0 +1,8 @@
+package MetodaSzablonowa;
+
+public class Kuchnia {
+    public static void main(String[] args) {
+        MakaronZBoczkiem makaronZBoczkiem = new MakaronZBoczkiem();
+        makaronZBoczkiem.recepturaMakaronu();
+    }
+}
---
+++ src/MetodaSzablonowaREADY/Demo.java
@@ -0,0 +1,8 @@
+package MetodaSzablonowaREADY;
+
+public class Demo {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+}
